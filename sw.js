bd40209a734ce19a2a4af6a1eaa37064d3ea5309
philippmilos345-hue <philipp.milos345@gmail.com>
2026-08-21 12:00:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,51 @@
-const CACHE='worthswitch-v5-2';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-));
-self.addEventListener('fetch',e=>e.respondWith(
-  fetch(e.request)
-    .then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy));
-      return resp;
-    })
-    .catch(()=>caches.match(e.request).then(r=>r||caches.match('./')))
-));
+const CACHE='worthswitch-v5-3';
+
+const ASSETS=[
+  './',
+  'index.html',
+  'styles.css',
+  'app.js',
+  'manifest.webmanifest',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys
+          .filter(key=>key!==CACHE)
+          .map(key=>caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request)
+      .then(response=>{
+        const copy=response.clone();
+
+        caches.open(CACHE).then(cache=>{
+          cache.put(e.request,copy);
+        });
+
+        return response;
+      })
+      .catch(()=>
+        caches.match(e.request).then(
+          cached=>cached||caches.match('./')
+        )
+      )
+  );
+});
